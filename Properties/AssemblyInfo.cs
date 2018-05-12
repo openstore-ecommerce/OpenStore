803dde5435af6d2b996604a121d54baf1bdfe3eb
leedavi <dcl@nevoweb.com>
2018-05-12 16:51:04 +0200

    v4.0.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenStore")]
-[assembly: AssemblyDescription("E-Commerce using DNN CMS")]
+[assembly: AssemblyDescription("E-Commerce with DNN CMS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nevoweb")]
 [assembly: AssemblyProduct("OpenStore")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.6")]
-[assembly: AssemblyFileVersion("4.0.0.6")]
+[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
