8bfed791040ffd6c8b1644bd0b34a255a90f8100
David Lee <dcl@nevoweb.com>
2018-10-11 14:32:06 +0200

    use first plugin in list as backoffice default.

--- Admin/Container.ascx.cs
+++ Admin/Container.ascx.cs
@@ -56,7 +56,13 @@ namespace Nevoweb.DNN.NBrightBuy.Admin
 
                     if (String.IsNullOrEmpty(ctrl))
                     {
-                        ctrl = "dashsummary";
+                        var plist = PluginUtils.GetPluginList();
+                        if (plist.Count() > 0)
+                        {
+                            // get first plugin in list as default.
+                            ctrl = plist.First().GUIDKey;
+                        }
+                        if (ctrl == "") ctrl = "dashsummary";
                         if (StoreSettings.Current.Settings().Count == 0) ctrl = "settings";
                         HttpContext.Current.Session["nbrightbackofficectrl"] = ctrl;
                     }
